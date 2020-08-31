0c9c35e961750d0823e07292af47f91ff84617be
Asaga Kosho <asagatto777@gmail.com>
2020-09-01 02:06:12 +0900

    Add: custom taxonomy and post type

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         </div>
       </section>
 
-      <!-- カテゴリ-->
+      <!-- カテゴリ
       <section class="text-gray-700 body-font">
         <div class="container px-5 py-24 mx-auto">
           <div class="flex flex-col">
@@ -71,7 +71,7 @@
             </div>
           </div>
         </div>
-      </section>
+      </section> -->
 
       <!-- carriculum -->
       <section class="text-gray-700 body-font">
@@ -90,56 +90,43 @@
             </div>
           </div>
           <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+          <?php 
+            $args = array(
+              'parent' => 0,
+              'taxonomy' => 'cource',
+              'order_by' => 'id',
+              'hide_empty' => 0
+            );
+            $cources = get_categories($args);
+            foreach($cources as $cource):
+              // var_dump($cource);
+          ?>
+          
             <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
               <div class="rounded-lg h-64 overflow-hidden">
                 <img alt="content" class="object-cover object-center h-full w-full"
-                  src="https://dummyimage.com/1203x503">
+                  src="<?=get_field('cource_image','cource_'.$cource->term_id);?>">
               </div>
-              <h2 class="text-xl font-medium title-font text-gray-900 mt-5">項目1</h2>
-              <p class="text-base leading-relaxed mt-2">あああ
+              <h2 class="text-xl font-medium title-font text-gray-900 mt-5"><?=$cource->name?></h2>
+              <p class="text-base leading-relaxed mt-2"><?=$cource->description?>
               </p>
-              <a class="text-blue-500 inline-flex items-center mt-3">学習する
+              <a class="text-blue-500 inline-flex items-center mt-3" href="<?=get_category_link( $cource->term_id )?>">学習する
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
               </a>
             </div>
-            <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-              <div class="rounded-lg h-64 overflow-hidden">
-                <img alt="content" class="object-cover object-center h-full w-full"
-                  src="https://dummyimage.com/1204x504">
-              </div>
-              <h2 class="text-xl font-medium title-font text-gray-900 mt-5">項目2</h2>
-              <p class="text-base leading-relaxed mt-2">あああ
-              </p>
-              <a class="text-blue-500 inline-flex items-center mt-3">学習する
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
+          
+            <?php endforeach; ?>
             </div>
-            <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-              <div class="rounded-lg h-64 overflow-hidden">
-                <img alt="content" class="object-cover object-center h-full w-full"
-                  src="https://dummyimage.com/1205x505">
-              </div>
-              <h2 class="text-xl font-medium title-font text-gray-900 mt-5">項目3</h2>
-              <p class="text-base leading-relaxed mt-2">あああ
-              </p>
-              <a class="text-blue-500 inline-flex items-center mt-3">学習する
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
         </div>
       </section>
 
       <!-- news-->
+      <?php 
+        if(have_posts()):
+      ?>
       <section class="text-gray-700 body-font overflow-hidden">
         <div class="container px-5 py-24 mx-auto">
           <div class="-my-8">
@@ -151,18 +138,24 @@
                 <h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">ニュース
                 </h1>
                 <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">
-                  ディスクリプション</p>
+                  更新情報をお届けします。</p>
               </div>
             </div>
-            <div class="py-8 flex flex-wrap md:flex-no-wrap">
+            <?php 
+              $i = 0;
+              while(have_posts()):
+                the_post();
+                $i++;
+            ?>
+            <div class="py-8 flex flex-wrap md:flex-no-wrap <?php if($i>=2) {echo("border-t-2 border-gray-200");} ?>">
               <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="tracking-widest font-medium title-font text-gray-900">カテゴリ</span>
-                <span class="mt-1 text-gray-500 text-sm">2020.01.01</span>
+                <span class="tracking-widest font-medium title-font text-gray-900"><?=the_category()?></span>
+                <span class="mt-1 text-gray-500 text-sm"><?=the_date("Y.m.d")?></span>
               </div>
               <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">ニュース1</h2>
-                <p class="leading-relaxed">ディスクリプション1</p>
-                <a class="text-blue-500 inline-flex items-center mt-4">詳しく見る
+                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2"><?=the_title()?></h2>
+                <p class="leading-relaxed"><?php echo mb_substr(strip_tags($post-> post_content),0,40) . '...'; ?></p>
+                <a class="text-blue-500 inline-flex items-center mt-4" href="<?=the_permalink()?>">詳しく見る
                   <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
                     stroke-linecap="round" stroke-linejoin="round">
                     <path d="M5 12h14"></path>
@@ -171,45 +164,11 @@
                 </a>
               </div>
             </div>
-            <div class="py-8 flex border-t-2 border-gray-200 flex-wrap md:flex-no-wrap">
-              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="tracking-widest font-medium title-font text-gray-900">カテゴリ</span>
-                <span class="mt-1 text-gray-500 text-sm">2020.01.01</span>
-              </div>
-              <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">ニュース2</h2>
-                <p class="leading-relaxed">ディスクリプション2</p>
-                <a class="text-blue-500 inline-flex items-center mt-4">詳しく見る
-                  <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
-                    stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-            </div>
-            <div class="py-8 flex border-t-2 border-gray-200 flex-wrap md:flex-no-wrap">
-              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="tracking-widest font-medium title-font text-gray-900">カテゴリ</span>
-                <span class="mt-1 text-gray-500 text-sm">2020.01.01</span>
-              </div>
-              <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">ニュース3
-                </h2>
-                <p class="leading-relaxed">ディスクリプション3</p>
-                <a class="text-blue-500 inline-flex items-center mt-4">詳しく見る
-                  <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
-                    stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
-              </div>
-            </div>
+            <?php endwhile ?>
           </div>
         </div>
       </section>
-
+      <?php endif; ?>
       <!-- minmoji : 読み込み遅くなるので検討 -->
       <!-- <section class="text-gray-700 body-font">
         <div class="container mx-auto flex flex-wrap">
